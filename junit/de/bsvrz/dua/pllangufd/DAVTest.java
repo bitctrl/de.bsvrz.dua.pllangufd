edfaa83e6237256d0af2f165e7c860370e293a3e
tfelder <info@bitctrl.de>
2008-01-29 15:35:40 +0000

    KB-Inovat V.21 importiert
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.pllangufd@6011 40c0be50-5d29-0410-901d-fb59edb5cafc

--- junit/de/bsvrz/dua/pllangufd/DAVTest.java
+++ junit/de/bsvrz/dua/pllangufd/DAVTest.java
@@ -50,7 +50,7 @@ public class DAVTest {
 	private static final String[] CON_DATA = new String[] {
 			"-datenverteiler=localhost:8083", //$NON-NLS-1$ 
 			"-benutzer=Tester", //$NON-NLS-1$
-			"-authentifizierung=c:\\passwd", //$NON-NLS-1$
+			"-authentifizierung=passwd", //$NON-NLS-1$
 			"-debugLevelStdErrText=CONFIG", //$NON-NLS-1$
 			"-debugLevelFileText=CONFIG" }; //$NON-NLS-1$
 

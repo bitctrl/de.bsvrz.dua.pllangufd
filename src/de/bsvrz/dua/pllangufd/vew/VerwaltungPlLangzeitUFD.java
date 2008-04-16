afac7ee822cd6203cd5090d5eb81976ea7a9f7cf
tfelder <info@bitctrl.de>
2008-04-16 08:57:52 +0000

    DefaultUncaughtExceptionHandler verschoben (jetzt sollte wieder alles abstürzen, wenn ein Thread abstürzt)
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.pllangufd@8285 40c0be50-5d29-0410-901d-fb59edb5cafc

--- src/de/bsvrz/dua/pllangufd/vew/VerwaltungPlLangzeitUFD.java
+++ src/de/bsvrz/dua/pllangufd/vew/VerwaltungPlLangzeitUFD.java
@@ -198,16 +198,6 @@ public class VerwaltungPlLangzeitUFD extends AbstraktVerwaltungsAdapter {
 	 *            Argumente der Kommandozeile
 	 */
 	public static void main(String[] argumente) {
-		Thread
-				.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-					public void uncaughtException(@SuppressWarnings("unused")
-					Thread t, Throwable e) {
-						Debug.getLogger().error("Applikation wird wegen" + //$NON-NLS-1$
-								" unerwartetem Fehler beendet", e); //$NON-NLS-1$
-						e.printStackTrace();
-						Runtime.getRuntime().exit(0);
-					}
-				});
 		StandardApplicationRunner.run(new VerwaltungPlLangzeitUFD(), argumente);
 	}
 
